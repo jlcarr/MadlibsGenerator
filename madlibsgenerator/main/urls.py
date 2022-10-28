188036429e23f64b8e7680398bee8caeabae7b78
Joseph Carr <joseph.carr@mail.mcgill.ca>
2022-10-28 17:45:45 -0400

    Filled views for playing the madlibs

--- madlibsgenerator/main/urls.py
+++ madlibsgenerator/main/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'main'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('<int:madlib_id>/', views.play_madlib, name='play'),
+	path('<int:madlib_id>/fill/', views.fill_madlib, name='fill'),
 	path('generate', views.generate_madlib, name='generate_madlib'),
 ]
--- madlibsgenerator/main/views.py
+++ madlibsgenerator/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import BaseText
 
@@ -9,6 +10,19 @@ def index(request):
 def generate_madlib(request):
 	return HttpResponse(request.POST['basetext'])
 
+
 def play_madlib(request, madlib_id):
-	substitution_set = BaseText.objects.get(pk=madlib_id).substitution_set.all()
-	return HttpResponse(str([s.__str__() for s in substitution_set]))
+	basetext = get_object_or_404(BaseText, pk=madlib_id)
+	substitution_set = basetext.substitution_set.all()
+	context = {
+		'basetext': basetext,
+		'substitution_list': substitution_set,
+	}
+	return render(request, "main/play.html", context)
+
+def fill_madlib(request, madlib_id):
+	basetext = get_object_or_404(BaseText, pk=madlib_id)
+	context = {
+		'text': basetext.text,
+	}
+	return render(request, "main/filled.html", context)
